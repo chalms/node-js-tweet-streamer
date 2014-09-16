bc04b19c797eccd3d115050b5eba44d5e6c8d2b6
Jaclyn Foss <andrew.n.chalmers@gmail.com>
2014-09-16 04:56:18 -0700

    dope shit

--- spec/topsy-spec.js
+++ spec/topsy-spec.js
@@ -11,6 +11,9 @@ describe('Topsy', function () {
     }
 
     outputLinks = ["http://twitter.com/Yahoo/status/425127302733656064", "http://twitter.com/CNN/status/422694719680827392", "http://twitter.com/Yahoo/status/421795726822092802", "http://twitter.com/msnbc/status/425363652674588672", "http://twitter.com/lilduval/status/428521075274551296", "http://twitter.com/manugavassi/status/430283695493513216", "http://twitter.com/nytimes/status/436602509210447872", "http://twitter.com/HugoGloss/status/433846184160296960", "http://twitter.com/nytimes/status/422867945476616192", "http://twitter.com/HuffingtonPost/status/428706901640359937"];
-    expect(Topsy.launch(json)).toEqual(outputLinks);
+    function done(data) {
+      console.log(data);
+    }
+    expect(Topsy.launch(json, "avatar", done)).toEqual(outputLinks);
   });
 });
--- topsy.js
+++ topsy.js
@@ -1,9 +1,13 @@
 var request = require('request');
 var phantom = require('phantom');
 var formatter = require('./util/formatter.js');
+var cheerio = require('cheerio');
 
 
-exports.launch = function (jsonStr) {
+exports.launch = function (jsonStr, collection, otherFn) {
+  _this = this;
+  _this.collection = collection;
+  _this.biggerCallback = otherFn;
 
   function buildUrl() {
     var query = jsonStr["q"];
@@ -11,14 +15,12 @@ exports.launch = function (jsonStr) {
      var offset = "";
      var timeString = "";
      var withTime = false;
-
      if (jsonStr["since"] !== undefined || jsonStr['since'] != null) {
         var value = parseInt((new Date(jsonStr['since'])).getTime()/1000);
         timeString = "&mintime=" + value;
         withTime = true;
      }
      if (jsonStr["until"] !== undefined || jsonStr['until'] != null) {
-
         var value = parseInt((new Date(jsonStr['until'])).getTime()/1000);
         if (withTime == false) {
           var nextDate = new Date();
@@ -31,51 +33,67 @@ exports.launch = function (jsonStr) {
      if (withTime) timeString = "%20" + timeString;
      return "http://topsy.com/s?q=" + query + timeString;
   }
-    var stack = [];
-    phantom.create(function (ph) {
-      phantom.state = [];
-      ph.createPage(function (page) {
-        page.open(buildUrl(), function (status) {
-          page.evaluate(function () {
-            $('#results div .media .media-body .inline li small a').each(function () {
-              if (($(this).attr('href').indexOf("status") > -1) && ($(this).attr('href').indexOf("trackback") === -1)) {
-                phantom.state.push($(this).attr('href'));
-                console.log("pushing!");
-              }
-            });
-          });
-          ph.exit();
-        });
+
+  function passBackStack(data) {
+
+    var jsonStack = [];
+    while(data.length > 0) {
+      var str = "https://api.twitter.com/1/statuses/oembed.json?id=";
+      var idString = data.pop().split("/status/")[1];
+      var newUrl = str + idString;
+      request(newUrl, function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+          console.log(html);
+          var theJSON = JSON.parse(html);
+          console.log(theJSON);
+          jsonStack.push(theJSON);
+        }
       });
+    }
+    return jsonStack;
+  }
+
+  function runIt(fn) {
+      mongoClient(fn(), _this.collection, function (data_result) {
+      console.log("~~~~~~~~~~~ the mongo data result is below ~~~~~~~");
+      console.log(data_result);
+      if (data_result) {
+        _this.biggerCallback("{ status: 200}");
+      }
+      return;
     });
+  }
+
+  var stack = [];
+  phantom.create(function(ph) {
+    return ph.createPage(function(page) {
+      return page.open(buildUrl(), function(status) {
+        console.log("opened site? ", status);
+              page.injectJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function() {
+                  setTimeout(function() {
+                      return page.evaluate(function() {
+                        var stack = [];
+                        $('#results div .media .media-body .inline li small a').each(function () {
+                            if (($(this).attr('href').indexOf("status") > -1) && ($(this).attr('href').indexOf("trackback") === -1)) {
+                              stack.push($(this).attr('href'));
+                            }
+                          });
+                          return stack;
+                      }, function(result) {
+                          console.log(result);
+                          runIt(function () {
+                            return passBackStack(result);
+                          });
+                          ph.exit();
+                      });
+                  }, 5000);
+
+              });
+          });
+      });
+  });
 
   console.log(phantom.state);
   console.log(stack);
-
-
-
-
-  // request(buildUrl(), function(error, response, html){
-  //   if(!error){
-  //     var $ = cheerio.load(html);
-  //     console.log(html);
-  //     console.log($);
-  //     $(document).ready(function () {
-  //       console.log("READY!")
-  //     });
-  //   }
-  // })
-  //     var stack = [];
-  //     $('#results div .media .media-body .inline li small a').each(function () {
-  //       if (($(this).attr('href').indexOf("status") > -1) && ($(this).attr('href').indexOf("trackback") === -1)) {
-  //         stack.push($(this).attr('href'));
-  //       }
-  //     })
-  //     for (var i in stack) {
-  //       console.log(stack[i]);
-  //     }
-  //     return stack;
-  //   }
-  // })
 }
 
